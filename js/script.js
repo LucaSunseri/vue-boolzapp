72c6cb89223a4c38e4c5f3c88c3debadbcb8b54a
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-16 14:15:50 +0100

    Added date

--- js/script.js
+++ js/script.js
@@ -2,12 +2,20 @@
   Milestone 1
   Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
   Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+*/
+/*
   Milestone 2
   Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
   Click sul contatto mostra la conversazione del contatto cliccato
+*/
+/*
   Milestone 3
   Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
   Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+*/
+/*
+  Milestone 4
+  Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
 */
 
 const app = new Vue({
@@ -147,11 +155,11 @@ const app = new Vue({
 
     addNewMessage(index) {
       
-      console.log('Proviamo a prendere l\'input', this.newMessageText);
+      // console.log('Proviamo a prendere l\'input', this.newMessageText);
 
       if (this.newMessageText.length > 0) {
         const newMessage = {
-          date: 'proviamo',
+          date: this.newDateMessage(),
           text: this.newMessageText,
           status: 'sent'
         }
@@ -162,7 +170,7 @@ const app = new Vue({
 
         setTimeout(() => {
           const newMessage = {
-            date: 'proviamo',
+            date: this.newDateMessage(),
             text: 'Funziona',
             status: 'received'
           }
@@ -171,8 +179,14 @@ const app = new Vue({
       }
 
 
+    },
+
+    newDateMessage() {
+      return new Date().toLocaleString();
     }
 
   }
 
 });
+
+
